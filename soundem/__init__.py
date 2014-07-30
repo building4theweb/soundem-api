6cb80e9e374245677f564aeea6f1b12ee965a9f1
José Padilla <jpadilla@webapplicate.com>
2014-07-30 07:48:23 -0400

    WIP setup for Flask-Security

--- soundem/__init__.py
+++ soundem/__init__.py
@@ -10,7 +10,12 @@ app = Flask(__name__)
 BASE_PATH = os.path.dirname(os.path.dirname(__file__))
 DATABASE_URI = 'sqlite:///{}/soundem.db'.format(BASE_PATH)
 
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'super-secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+
+# Flask-Security Config
+app.config['SECURITY_CONFIRMABLE'] = False
 
 # Initialize database
 db = SQLAlchemy(app)
