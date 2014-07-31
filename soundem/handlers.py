7191926318b1ac8899b799b43714ea3e135ceddd
José Padilla <jpadilla@webapplicate.com>
2014-07-30 23:03:43 -0400

    Add 405 error handler

--- soundem/handlers.py
+++ soundem/handlers.py
@@ -3,8 +3,7 @@ from flask import jsonify
 from soundem import app
 
 
-@app.errorhandler(400)
-def bad_request_handler(e):
+def json_error_handler(e):
     return jsonify({
         'status_code': e.code,
         'error': 'Bad Request',
@@ -12,28 +11,21 @@ def bad_request_handler(e):
     }), e.code
 
 
+@app.errorhandler(400)
+def bad_request_handler(e):
+    return json_error_handler(e)
+
+
 @app.errorhandler(401)
 def unauthorized_handler(e):
-    return jsonify({
-        'status_code': e.code,
-        'error': 'Unauthorized',
-        'detail': e.description
-    }), e.code
+    return json_error_handler(e)
 
 
 @app.errorhandler(404)
 def not_found_handler(e):
-    return jsonify({
-        'status_code': e.code,
-        'error': 'Not Found',
-        'detail': e.description
-    }), e.code
+    return json_error_handler(e)
 
 
 @app.errorhandler(405)
 def method_not_allowed_handler(e):
-    return jsonify({
-        'status_code': e.code,
-        'error': 'Method Not Allowed',
-        'detail': e.description
-    }), e.code
+    return json_error_handler(e)
