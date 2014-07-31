5332997b73132fc8bf40cbbd7aaf48d8b24e399f
José Padilla <jpadilla@webapplicate.com>
2014-07-30 21:13:34 -0400

    Return correct code in error handlers

--- soundem/handlers.py
+++ soundem/handlers.py
@@ -6,25 +6,25 @@ from soundem import app
 @app.errorhandler(400)
 def bad_request_handler(e):
     return jsonify({
-        'status_code': 400,
+        'status_code': e.code,
         'error': 'Bad Request',
         'detail': e.description
-    })
+    }), e.code
 
 
 @app.errorhandler(401)
 def unauthorized_handler(e):
     return jsonify({
-        'status_code': 401,
+        'status_code': e.code,
         'error': 'Unauthorized',
         'detail': e.description
-    })
+    }), e.code
 
 
 @app.errorhandler(404)
 def not_found_handler(e):
     return jsonify({
-        'status_code': 404,
+        'status_code': e.code,
         'error': 'Not Found',
         'detail': e.description
-    })
+    }), e.code
